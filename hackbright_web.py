ac71a7e43e024ec346f4d67a526d1ffdfd024aba
Sam Gillis <sbvb13@gmail.com>
2020-10-21 21:49:10 +0000

    Created student creation form to add student to the database
    Co-authored-by: kayteecannon <kaytee.cannon@me.com>

--- hackbright_web.py
+++ hackbright_web.py
@@ -25,6 +25,30 @@ def get_student_form():
 
     return render_template("student_search.html")
 
+
+@app.route("/student_add")
+def student_add():
+    """Add a new student form"""
+
+    return render_template("student_add.html")
+
+@app.route("/student_added", methods=['POST'])
+def add_student_to_database():
+    """Adds new student to database"""
+
+    first = request.form.get('first')
+    last = request.form.get('last')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first, last, github)
+
+    first, last, github = hackbright.get_student_by_github(github)
+
+    return render_template("student_added.html", first=first, 
+    last=last, github=github)
+
+
+
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True, host="0.0.0.0")
